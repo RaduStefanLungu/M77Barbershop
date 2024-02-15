cb7c1006d7b1f737b28959670ffc76e9b432ef0d
RaduStefanLungu <radustefanlungu@hotmail.com>
2024-02-15 20:00:05 +0100

    started appointment

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -24,37 +24,6 @@ export const auth = getAuth(app)
 const firestore_db = getFirestore(app)
 
 
-export async function checkIfAdmin(userEmail){
-  const myDoc = await getDocumentById('misc','admin_accounts')
-    
-  try{
-
-    if(myDoc.emails.includes(userEmail)){
-      return true
-    }
-    else return false
-
-  }catch(err){
-    console.log(err.message);
-  }
-
-}
-
-export async function checkIfBarman(userEmail){
-  const myDoc = await getDocumentById('misc','barman_accounts')
-    
-  try{
-
-    if(myDoc.emails.includes(userEmail)){
-      return true
-    }
-    else return false
-
-  }catch(err){
-    console.log(err.message);
-  }
-
-}
 export async function getUserDocument(userEmail){
   const myCollection = await collection(firestore_db,"user_data")
   const querySnapshot = await getDocs(myCollection)
@@ -74,24 +43,26 @@ export async function getUserDocument(userEmail){
   return searchedDocument;
 }
 
-export async function getUserDocumentByID(userID){
-  const myCollection = await collection(firestore_db,"user_data")
-  const querySnapshot = await getDocs(myCollection)
+// export async function getUserDocumentByID(userID){
+//   const myCollection = await collection(firestore_db,"user_data")
+//   const querySnapshot = await getDocs(myCollection)
   
-  const documentsData = []
-  querySnapshot.forEach((doc) => {
-    documentsData.push({id: doc.id, ...doc.data()})
-  })  
+//   const documentsData = []
+//   querySnapshot.forEach((doc) => {
+//     documentsData.push({id: doc.id, ...doc.data()})
+//   })  
 
-  let searchedDocument = null
-  documentsData.forEach((doc) => {
-    if(doc.id === userID){
-      searchedDocument = doc
-      return false
-    }
-  })
-  return searchedDocument;
-}
+//   let searchedDocument = null
+//   documentsData.forEach((doc) => {
+//     if(doc.id === userID){
+//       searchedDocument = doc
+//       return false
+//     }
+//   })
+//   return searchedDocument;
+// }
+
+
 
 const getDocumentById = async (collectionName, documentId) => {
   try {
@@ -122,6 +93,47 @@ export async function getUsers(){
 
   return all_users;
 }
+
+export async function getAppointmentDate(documentID) {
+    try{
+        return getDocumentById("appointments",documentID)
+    }catch(e){
+        addError({
+            e_message: "Failed to get appointment from << appointments >> firestore table.",
+            program_execution: "Failed to execute firebase.getAppointmentDate(...)",
+            program_function_error: `addAppointment(${documentID})`,
+            program_page: "/rendez-vous",
+          })
+          return(e)
+    }
+}
+
+export async function addAppointment(user_name,user_emai,user_phone,rdv_date,rdv_time){
+    const data = {
+        user_name: user_name,
+        user_emai: user_emai,
+        user_phone: user_phone,
+        rdv_date: rdv_date,
+        rdv_time: rdv_time,
+        rdv_taken_time : Timestamp.now()
+    }
+  
+    try{
+      addDoc(collection(firestore_db,"appointments"),data)
+      return(true)
+
+    }catch(e){
+      addError({
+        e_message: "Failed to add appointment to << appointments >> firestore table.",
+        program_execution: "Failed to execute firebase.addAppointment(...)",
+        program_function_error: `addAppointment(${user_name},${user_emai},${user_phone},${rdv_date},${rdv_time})`,
+        program_page: "/rendez-vous",
+      })
+      return(e)
+    }
+  
+  }
+
 
 export async function addUser(email,phone,sex,caracteristics,newsletterSubscription,acceptedTerms,accountCompleted){
   const data = {
@@ -180,208 +192,12 @@ export async function updateUser(docID,data){
   
 }
 
-export async function getNotifications(){
-  // Allowed : all users
-  const myCollection = await collection(firestore_db,"notifications")
-  const querySnapshot = await getDocs(myCollection)
-
-  const all_notifications = []
-  querySnapshot.forEach((doc) => {
-    all_notifications.push({id: doc.id, ...doc.data()})
-  })  
-
-  return all_notifications;
-}
-
-export async function addNotification(title,message){
-  // Allowed : admin/dev
-
-  const data = {
-    title: title,
-    message: message,
-    expired: false,
-    sent_to_users : false,
-    time_stamp: Timestamp.now()
-  }
-
-  try{
-    addDoc(collection(firestore_db,"notifications"),data)
-    return('Notification successfully added !')
-  }catch(e){
-    addError({
-      e_message: "Failed to add notification to notifications firestore table.",
-      program_execution: "Failed to execute firebase.addNotification(...)",
-      program_function_error: `addUser(${title},${message})`,
-      program_page: "-",
-      user_email: `-`
-    })
-    return(e)
-  }
-}
-
-
-export async function removeNotification(notificationID) {
-  try {
-    // Create a reference to the document to be deleted
-    const notificationRef = doc(firestore_db, 'notifications', notificationID);
-
-    // Delete the document
-    await deleteDoc(notificationRef);
-
-    console.log('Notification removed successfully');
-  } catch (error) {
-    addError({
-      e_message: "Failed to remove notification from notifications firestore table.",
-      error : `${error.message}`,
-      program_execution: "Failed to execute firebase.removeNotification(notiifcationID)",
-      program_function_error: `removeNotification(${notificationID})`,
-      program_page: "/admin/dashboard -> notificationsView",
-      user_document_id: `${notificationID}`
-    })
-    console.log(error);
-    return(error)
-  }
-}
-
-export async function updateNotification(docID,data){
-  const documentRef = doc(firestore_db,"notifications",docID)
-
-  updateDoc(documentRef,data).then(
-    () => {
-      console.log('successfully updated notification');
-      return(true)
-    }
-  ).catch(
-    (error) => {
-      addError({
-        e_message: "Failed to update data to notifications firestore table.",
-        error : `${error.message}`,
-        program_execution: "Failed to execute firebase.updateNotification(docID,data)",
-        program_function_error: `updateNotification(${docID},${data})`,
-        program_page: "/admin/dashboard -> notificationsView",
-        user_document_id: `${docID}`
-      })
-      console.log(error);
-      return(error)
-    }
-  )
-
-  
-}
-
-export async function sendNotificationsToUsers(notificationsIds){
-  //adds notification to user.notifications IF notification.id is not in user.notifications and if notification.expired = false
-
-  const usersCollection = await collection(firestore_db,"user_data")
-  const querySnapshotUsers = await getDocs(usersCollection)
-
-  // get all notifications from notifications
-  const all_notifications = await getNotifications()
-  // const updated_notifications_list = all_notifications.map((item) => ({ ...item, seen: false }));      // adds all notifications !!
-  const updated_notifications_list = all_notifications
-  .filter((item) => notificationsIds.includes(item.id))
-  .map((item) => ({ ...item, seen: false }));
-
-  // update each user notifications list
-  querySnapshotUsers.forEach((user) => {
-    const users_notifications = user.data().notifications
-
-    const updatedUsersNotifications = [
-      ...users_notifications,
-      ...updated_notifications_list.filter(
-        (updatedNotification) =>
-          updatedNotification.expired === false &&
-          !users_notifications.some((userNotification) => userNotification.id === updatedNotification.id)
-      ),
-    ];
-
-    updateUser(user.id,{notifications:updatedUsersNotifications})
-  })  
-}
-export async function clearUsersNotifications(){
-  const usersCollection = await collection(firestore_db,"user_data")
-  const querySnapshotUsers = await getDocs(usersCollection)
-
-  const notificationsCollection = await collection(firestore_db,"notifications")
-  const querySnapshotNotifications = await getDocs(notificationsCollection)
-
-
-  // update users
-  querySnapshotUsers.forEach((user) =>{
-    updateUser(user.id,{notifications:[]})
-  })
-
-  // update notification
-
-  querySnapshotNotifications.forEach((notification) => {
-    updateNotification(notification.id,{sent_to_users:false})
-  })
-
-}
-
-export async function getCountdownTime(){
-  const countdownDoc = await getDocumentById("misc","CountdownTime");
-  return(`${countdownDoc.date}-${countdownDoc.hour}`)
-  
-}
-
-
-export async function getUserCaracteristics(userDocumentID){
-
-  const document = await getDocumentById('user_data',userDocumentID)
-
-  if(document !== null){
-    return(document.caracteristics)
-  }
-
-}
-
-export async function getMatchDocumentByID(matchID){
-  const myCollection = await collection(firestore_db,"matching_table")
-  const querySnapshot = await getDocs(myCollection)
-  
-  const documentsData = []
-  querySnapshot.forEach((doc) => {
-    documentsData.push({id: doc.id, ...doc.data()})
-  })  
-
-  let searchedDocument = null
-  documentsData.forEach((doc) => {
-    if(doc.id === matchID){
-      searchedDocument = doc
-      return false
-    }
-  })
-  return searchedDocument;
-}
-
-export async function getMatchDocumentByPersonID(p_id){
-  const myCollection = await collection(firestore_db,"matching_table")
-  const querySnapshot = await getDocs(myCollection)
-  
-  const documentsData = []
-  querySnapshot.forEach((doc) => {
-    documentsData.push({id: doc.id, ...doc.data()})
-  })  
-
-  let searchedDocument = null
-  documentsData.forEach((doc) => {
-    if(doc.p1_id === p_id || doc.p2_id === p_id){
-      searchedDocument = doc
-      return false
-    }
-  })
-  return searchedDocument;
-}
-
 export async function addError(data){
-  const error_data = {
-    error_time: Timestamp.now(),
-    error_message: data
+    const error_data = {
+      error_time: Timestamp.now(),
+      error_message: data
+    }
+    addDoc(collection(firestore_db,"error_table"),error_data)
   }
-  addDoc(collection(firestore_db,"error_table"),error_data)
-}
-
-
 
 // export default app;
--- client/src/pages/RendezVous.jsx
+++ client/src/pages/RendezVous.jsx
@@ -1,4 +1,9 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { addAppointment, getAppointmentDate } from '../firebase'
+
+import DATA from '../data/data.json'
+
+// TODO make user to add appointment , then render appointment if it is available
 
 export default function RendezVous() {
   return (
@@ -18,28 +23,93 @@ const RdvForm = () => {
   const [email,setEmail] = useState('')
   const [phone,setPhone] = useState('')
   const [date,setDate] = useState('')
+  const [appointmentTime, setAppointmentTime] = useState('')
+
+  const [today,setToday] = useState(new Date().toISOString().split('T')[0])
+
+  const [appointmentHours, setAppointmentHours] = useState([])
+
 
   // when date is set query from db all the free appointment spots
+
+  function handleDateChosen(e){
+    e.preventDefault()
+
+    // query
+
+    // console.log(e.target.value);
+    let given_date = e.target.value
+
+    let splitted_date = given_date.split('-')
+
+    let formatted_date = splitted_date[2] + "_" + splitted_date[1] + "_" + splitted_date[0]
+
+    console.log(formatted_date);
+
+    setDate(formatted_date)
+    
+    // user chosen day
+    let chosen_day_of_week = getDayOfWeek(new Date(e.target.value)) 
+    console.log(chosen_day_of_week);
+
+
+    DATA.horaire.map(
+      (value,key) =>{
+        if(value.day === chosen_day_of_week){
+          setAppointmentHours(value.hours)
+        }
+      }
+    )
+  
+  }
 
   function handleSubmit(e) {
     e.preventDefault()
 
-    
+    // add appointment
 
+    // addAppointment(fullName, email, phone, date, appointmentTime)
+
+    // console.log(generateAppointmentIntervals("9:00","20:00"));
+
+  }
+
+  function getDayOfWeek(default_date) {
+    if (default_date) {
+      const days = ['dimanche', 'lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi'];
+      const dayIndex = default_date.getDay();
+      return days[dayIndex];
+    } else {
+      return '';
+    }
+  };
+
+
+
+  const HourTab = ({Value,Free}) => {
+    return(
+      <button className={`${Free? "bg-green-300": "bg-red-300"}`}>
+        {Value}
+      </button>
+    )
   }
 
 
   return(
     <form className='grid bg-gray-300 py-10' onSubmit={handleSubmit}>
       <div className='grid gap-5'>
-        <input type='text' placeholder='Nom et Prénom' className='border-black border-2' onChange={(e)=>{setFullName(e.target.value)}}/>
-        <input type='text' placeholder='Email' className='border-black border-2' />
-        <input type='text' placeholder='GSM' className='border-black border-2' />
-        <input type='date' className='border-black border-2' />
+        <input type='text' required placeholder='Nom et Prénom' className='border-black border-2' onChange={(e)=>{setFullName(e.target.value)}}/>
+        <input type='text' required placeholder='Email' className='border-black border-2' onChange={(e)=>{setEmail(e.target.value)}}/>
+        <input type='text' required placeholder='GSM' className='border-black border-2' onChange={(e)=>{setPhone(e.target.value)}} />
+        <input type='date' id='chosen_date' required min={today} className='border-black border-2' onChange={handleDateChosen}/>
       </div>
 
-      <div className='py-5'>
-        ...
+      <div className='py-5 grid'>
+        {appointmentHours.map((value,key) => {
+          return(
+            <HourTab key={key} Value={value} Free={true} />
+          )
+        })}
       </div>
 
       <button type='submit' className='bg-red-300 py-2 m-4'>Réserver</button>
